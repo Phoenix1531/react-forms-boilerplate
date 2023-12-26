04b5deff3fb62060a41dbf3e2a1997c6f206b9ee
phoenix1531 <arjun15310@gmail.com>
2023-12-26 22:42:53 +0530

    done

---
+++ form/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Forms from './components/Forms'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Forms/>
+      {/*  React Toasting  */}
+    </>
+  )
+}
+
+export default App
